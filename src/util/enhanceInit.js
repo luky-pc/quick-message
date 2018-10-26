e251b666a049b051bc3a49f043903d136fb01f38
eleven <looker_pc@163.com>
2018-10-27 01:49:25 +0800

    通过缓存oldState，避免同一事务中多次触发监听事件

--- src/util/enhanceInit.js
+++ src/util/enhanceInit.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 React.Component.prototype.updateState=function(object,callback) {//使用updateState 方法替换setState 方法，添加对setState 值改变时监听
+    !this.oldState&&(this.oldState=JSON.parse(JSON.stringify(this.state)));
     this.setState(object,()=>{
         for(let key in this.watch){
-            if(key in object){
+            if((key in object)&&this.state[key]!==this.oldState[key]){
+                this.oldState[key]=this.state[key];//state[key]的值已更新到最新状态,避免同一事务中多次触发watch事件
                 this.watch[key]();
             }
         }
