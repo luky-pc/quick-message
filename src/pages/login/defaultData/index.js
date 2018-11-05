34e4735cba5de216948269d939b110f86a811cad
eleven <looker_pc@163.com>
2018-11-05 21:09:38 +0800

    假数据

---
+++ src/pages/login/defaultData/index.js
@@ -0,0 +1,5 @@
+let userList=[{nick:"全世界最帅",portrait:undefined,id:"666546"},
+    {nick: "eleven", portrait: undefined, id: "17298566362"},
+    { nick: "超酷", portrait: undefined, id: "17298566363"},
+    { nick: "熊二", portrait: undefined, id: "17298566364"}];
+export {userList};
