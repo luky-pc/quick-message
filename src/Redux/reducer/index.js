92202ea2ab48e12812ab6e1de96e233122b581fc
luky <looker_pc@163.com>
2019-01-22 17:17:36 +0800

    添加所有接口调用 传递当前用户电话号码信息

--- src/Redux/reducer/index.js
+++ src/Redux/reducer/index.js
@@ -13,7 +13,7 @@ let Reducer = (state=initStore,action)=>{
             });
             /*TODO:当前聊天记录中没有该联系人的处理情况，新建contact 对象*/
             message.actionType=actionTypes.SEND_MESSAGE;
-            ws.send(JSON.stringify(message));
+            ws.sendMessage(JSON.stringify(message));
             contact.message.push(message);
             return {...state, messageList};
         case actionTypes.RECEIVE_MESSAGE:
@@ -33,22 +33,22 @@ let Reducer = (state=initStore,action)=>{
             return {...state};/**TODO:更新未读信息（登录时初始化调用）**/
         case actionTypes.REGISTER_USER:
             userInfo.actionType=actionTypes.REGISTER_USER;
-            ws.send(JSON.stringify(userInfo));
+            ws.sendMessage(JSON.stringify(userInfo));
             return {...state};/**TODO:处理用户注册**/
         case actionTypes.RECEIVE_REGISTER_RESULT:
-            ws.send(JSON.stringify());
+            ws.sendMessage(JSON.stringify());
             return {...state};/**TODO:用户首次注册成功相关操作**/
         case actionTypes.LOGIN:
             userInfo.actionType=actionTypes.LOGIN;
-            ws.send(JSON.stringify(userInfo));
+            ws.sendMessage(JSON.stringify(userInfo));
             return {...state};/**TODO:处理登录**/
         case actionTypes.SEARCH_USER:
-            ws.send(JSON.stringify({actionType:actionTypes.SEARCH_USER,phoneNumber}));
+            ws.sendMessage(JSON.stringify({actionType:actionTypes.SEARCH_USER,phoneNumber}));
             return {...state};
         case actionTypes.RECEIVE_SEARCH_USER:
             return {...state,searchUserResult};
         case actionTypes.ADD_CONTACT:
-            ws.send(JSON.stringify({actionType:actionTypes.ADD_CONTACT,userPhoneNumber:state.userInfo.phoneNumber,phoneNumber}));
+            ws.sendMessage(JSON.stringify({actionType:actionTypes.ADD_CONTACT,userPhoneNumber:state.userInfo.phoneNumber,phoneNumber}));
             return {...state};
         case actionTypes.RECEIVE_ADD_CONTACT:
             let {messageList:msg}=state;
--- src/socket-connect/connect.js
+++ src/socket-connect/connect.js
@@ -2,7 +2,9 @@
  * 提供websocket服务
  */
 import {WSERVER} from "../config/config";
-let ws,logInfo={};
+ import objectCheck from "../util/objectCheck";
+import userService from "../service/userService";
+let ws,logInfo={},tempSend;
 if(window.WebSocket){
     ws = new WebSocket(WSERVER);
 
@@ -19,6 +21,16 @@ if(window.WebSocket){
 
     ws.onmessage = function(e){
         console.log(e.data);
+    }
+    ws.sendMessage=function(msg){
+        let userInfo=userService.getUserInfo();
+        if(objectCheck.isObject(msg)){
+            msg.currentUserPhoneNumber=userInfo.phoneNumber;
+        }else{
+            msg=JSON.parse(msg);
+            msg.currentUserPhoneNumber=userInfo.phoneNumber;
+        }
+        ws.send(JSON.stringify(msg));
     }
 }
 window.setInterval(function(){ //每隔5秒钟发送一次心跳，避免websocket连接因超时而自动断开
