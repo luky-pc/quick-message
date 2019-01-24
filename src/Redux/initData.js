e79ba04e9466844bbba2ae491b152098bc0353bd
luky <looker_pc@163.com>
2019-01-24 13:57:59 +0800

    清除模拟数据

--- src/Redux/initData.js
+++ src/Redux/initData.js
@@ -3,13 +3,8 @@ let userInfo=JSON.parse(sessionStorage.getItem("currentUser")||"{}");
 let initStore={
     userInfo,//{Object} 当前用户信息
     contactList:[],//联系人信息
-    searchUserResult:[{
-        nickName: "eleven",
-        portrait: undefined,
-        phoneNumber:"18180874439",
-        id: "17298566362"
-    }],//用户查找联系人返回结果
-    messageList:fakeMsgs//用户和联系人等对象的消息列表
+    searchUserResult:[],//用户查找联系人返回结果
+    messageList:[]//用户和联系人等对象的消息列表
 };
 
 export {initStore};
